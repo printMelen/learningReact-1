5615edf1742db9e4854eafff9b15781ab58bae7a
printMelen <alvaro.redondofgb@gmail.com>
2024-02-12 21:34:32 +0100

    Intentando meter tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card.jsx";
+
+function App() {
+  return (
+    <>
+      <Card />
+      <h1>Hello, world!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+
+function Card() {
+    return (
+      <>
+        <h1 className="text-red-400">Hello, world!</h1>
+      </>
+    );
+  }
+  
+  export default Card;
+  
