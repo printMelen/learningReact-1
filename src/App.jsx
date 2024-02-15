2ac1da7581fd29444cb80683ea29fef3794ff535
printMelen <alvaro.redondofgb@gmail.com>
2024-02-15 09:57:44 +0100

    Añadiendo el usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-
+import Usuario from './Usuario.jsx'
 function App() {
 
   return (
     <>
+    <Usuario ruta="public\Rubik's_cube.svg.png" name="Álvaro"></Usuario>
     <h1 className='text-3xl font-bold text-red-500 underline text-center'>Funca?</h1>
     </>
   )
